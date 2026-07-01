0a96612653aaacfcce4a6050d34355cad0935a95
ComplexSpaces <code@pengin.systems>
2026-07-01 16:58:07 -0500

    Enable compilation on all Apple targets
    The mach APIs used are available and non-restricted on watchOS and visionOS as well

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::num::NonZeroUsize;
 
 #[cfg_attr(any(target_os = "linux", target_os = "android"), path = "linux.rs")]
 #[cfg_attr(target_os = "freebsd", path = "freebsd.rs")]
-#[cfg_attr(any(target_os = "macos", target_os = "ios"), path = "apple.rs")]
+#[cfg_attr(target_vendor = "apple", path = "apple.rs")]
 #[cfg_attr(target_os = "aix", path = "aix.rs")]
 mod imp;
 
